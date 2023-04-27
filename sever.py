2980d530f5986d8f1ea74dc183238dfc5e6274a7
wanne88 <125958804+wanne88@users.noreply.github.com>
2023-04-27 18:24:03 +0700

    Add files via upload

---
+++ sever.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from pyChatGPT import ChatGPT
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World!'
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.get_json()
+    text = ChatGPT(data['prompt'], data['length'])
+    return jsonify({'text': text})
+
+if __name__ == '__main__':
+    app.run()
