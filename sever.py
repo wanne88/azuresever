79a9acf84f10b5969e9debfcc41d306a1bbe1453
wanne88 <125958804+wanne88@users.noreply.github.com>
2023-04-27 22:40:42 +0700

    Update sever.py

--- sever.py
+++ sever.py
@@ -1,8 +1,9 @@
+from flask_cors import CORS
 from flask import Flask, request, jsonify
 from pyChatGPT import ChatGPT
 import openai
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def home():
     return 'Hello World!'
